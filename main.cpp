a985b5b0340f19639c4dfc8498d33032161d18ed
sergorl <serg.neworl@gmail.com>
2018-11-01 14:46:33 +0300

    add wait

--- main.cpp
+++ main.cpp
@@ -103,7 +103,7 @@ void multi(list<list<string>> coms) {
                 if( dup2(pipefds[commandc*2+1], 1) < 0 )
                     cout << "Error2:" << strerror(errno) << "\n";
             } else {
-                FILE* pFile = fopen("/home/box/result.out", "w");
+                FILE* pFile = fopen("home/box/result.out", "w");
                 int fd = fileno(pFile);
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
@@ -119,6 +119,8 @@ void multi(list<list<string>> coms) {
         } else if( pid < 0 ){
             cout << "Error:" << strerror(errno) << "\n";
         }
+
+        wait(NULL);
 
         ++commandc;
     }    
@@ -140,7 +142,7 @@ int main()
         auto com = coms(str);
         multi(com);
     } else {
-        FILE* pFile = fopen("/home/box/result.out", "w");
+        FILE* pFile = fopen("home/box/result.out", "w");
         fclose(pFile);
     }
 
